a4c13460c2e8e4c5dc5827536ffc0054aab8d022
TANABE Ken-ichi <nabeken@tknetworks.org>
2025-02-04 19:06:07 +0900

    fix: cosmetic change for the test

--- check_test.go
+++ check_test.go
@@ -2,21 +2,23 @@ package nagiosplugin
 
 import (
 	"fmt"
-	"math/rand"
 	"testing"
-	"time"
 )
 
 func TestCheck(t *testing.T) {
-	rand.Seed(time.Now().UTC().UnixNano())
-	c := NewCheck()
-	expected := "CRITICAL: 200000 terrifying space monkeys in the engineroom | space_monkeys=200000c;10000;100000;0;4294967296"
+	c := NewCheck("CHECK TEST")
+
+	expected := "CHECK TEST CRITICAL: 200000 terrifying space monkeys in the engineroom | space_monkeys=200000c;10000;100000;0;4294967296"
+
 	nSpaceMonkeys := float64(200000)
 	maxSpaceMonkeys := float64(1 << 32)
-	c.AddPerfDatum("space_monkeys", "c", nSpaceMonkeys, 0, maxSpaceMonkeys, 10000, 100000)
+
+	_ = c.AddPerfDatum("space_monkeys", "c", nSpaceMonkeys, 0, maxSpaceMonkeys, 10000, 100000)
 	c.AddResult(CRITICAL, fmt.Sprintf("%v terrifying space monkeys in the engineroom", nSpaceMonkeys))
+
 	// Check a WARNING can't override a CRITICAL
 	c.AddResult(WARNING, fmt.Sprintf("%v slightly annoying space monkeys in the engineroom", nSpaceMonkeys))
+
 	result := c.String()
 	if expected != result {
 		t.Errorf("Expected check output %v, got check output %v", expected, result)
